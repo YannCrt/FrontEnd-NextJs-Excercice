8f15ef5729711a55d0f394c46c0eba04a0c3fe73
YannCrt <yanncretin75@gmail.com>
2024-12-06 15:51:10 +0100

    add assets folder in  /src and use Ant icon
    -remove public folder
    -add assets folder in /src
    -add  UserOutlined icon with @ant-design/icons in /navbar/page.tsx

--- src/app/[locale]/components/navbar/page.tsx
+++ src/app/[locale]/components/navbar/page.tsx
@@ -3,6 +3,9 @@ import React, { useState } from "react";
 import { Button } from "antd";
 import {Link} from '@/i18n/routing';
 import {useTranslations} from 'next-intl';
+import { UserOutlined } from "@ant-design/icons";
+import Image from "next/image";
+import logo from '@/assets/logo.jpg'; 
 
 const Navbar: React.FC = () => {
   const [isRecruteur, setIsRecruteur] = useState(false);
@@ -11,7 +14,13 @@ const Navbar: React.FC = () => {
   return (
     <nav className="flex items-center justify-between bg-slate-800 p-4 text-white">
       <Link href="/" className="flex items-center justify-center gap-8">
-        <img src="/assets/logo.jpg" alt="Logo" className="h-10" />
+      <Image
+          src={logo} // logo est un objet StaticImageData
+          alt="Logo"
+          width={150}
+          height={50}
+          className="h-10 w-10"
+        />
         <span>Arimayi</span>
       </Link>
       <ul className="m-0 flex list-none items-center justify-center gap-8 p-0">
@@ -34,7 +43,7 @@ const Navbar: React.FC = () => {
           </li>
         )}
         <li className="m-0">
-         <a onClick={() => setIsRecruteur(!isRecruteur)} ><img className = "h-10 w-10 rounded-3xl"src="/assets/icon.png" ></img></a>
+         <a onClick={() => setIsRecruteur(!isRecruteur)} ><UserOutlined className = "h-10 w-10 rounded-3xl"></UserOutlined></a>
         </li>
       </ul>
     </nav>
